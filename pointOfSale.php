6aa101b2de76e98988a5f5e3410efbfd8ebdd468
Barnesey41 <obarnes23@hotmail.co.uk>
2021-11-16 20:41:27 +0000

    Implemented the addition of items to a transaction

--- pointOfSale.php
+++ pointOfSale.php
@@ -15,5 +15,126 @@
     <a style="float: right;" href="Login.php">Log-in</a>
 </div>
 
+<style>
+    span.leftGUI{
+        display: inline-block;
+        float: left;
+        width: 20%;
+        height: 655px;
+        padding-top: 10px;
+        border: 1px solid blue;
+        background-color: yellow;
+    }
+
+    span.saleItem{
+        display: inline-grid;
+        width: 22%;
+        height: 15%;
+        border: 1px solid blue;
+        background-color: yellow;
+        margin: 10px;
+    }
+
+    span.midGUI{
+        margin: auto;
+        display: inline-block;
+        width: 71%;
+        height: 655px;
+        padding-bottom: 5px;
+        padding-top: 13px;
+        padding-left 10px;
+        border: 1px solid blue;
+        background-color: yellow;
+        font-size: 18px; /* Cbange to zero, testing with above 0 */
+    }
+
+    span.rightGUI{
+        display: inline-block;
+        width: 8%;
+        height: 655px;
+        padding-top: 10px;
+        float:right;
+        border: 1px solid blue;
+        background-color: yellow;
+    }
+
+    button.saleItem{
+        display: inline-grid;
+        width: 22%;
+        height: 15%;
+        border: 1px solid blue;
+        background-color: yellow;
+        margin: 10px;
+    }
+</style>
+
+<div>
+    <span class='leftGUI'>
+        <?php
+
+            session_start();
+            $companyName = $_SESSION['companyName'];
+
+
+
+            /* Open Database Connection */
+            include "database_connect.php";
+            $connection = openConnection();
+
+            $query = "SELECT TransactionID FROM transactionhistory_$companyName WHERE Complete = 'no'";
+
+            $result = mysqli_query($connection,$query);
+            $row = mysqli_fetch_assoc($result);
+
+            $transactionID = $row['TransactionID'];
+
+            echo "<h2 align='center'> Transaction ID: ".$transactionID."</h2>";
+
+        ?>
+    </span>
+
+    <span class = 'midGUI' >
+        <?php
+        $menuName = $_SESSION['menuName'];
+
+        $query = "SELECT MenuID FROM menus WHERE MenuName = '$menuName' AND CompanyName = '$companyName'";
+        $result = mysqli_query($connection, $query);
+
+        $row = mysqli_fetch_assoc($result);
+        $menuID = $row['MenuID'];
+
+        $tableName = "menu_".$companyName."_".$menuID;
+
+        $query = "SELECT saleItemName,ID,price FROM $tableName";
+        $result = mysqli_query($connection, $query);
+        $numRows = mysqli_num_rows($result);
+
+        for($i=0;$i<$numRows; $i++){
+            $row = mysqli_fetch_assoc($result);
+            $saleItemName = $row['saleItemName'];
+            $saleItemID = $row['ID'];
+            $displayPrice = "£".$row['price'];
+            $price = $row['price'];
+
+            echo "<form action = 'process_buttonClick_saleItemAdd.php'>
+                    <input type='hidden' name='transactionID' value='$transactionID'>
+                    <input type='hidden' name='saleItemName' value='$saleItemName'> <!-- type= hidden means this is not visable but can send necissary data to processing--->
+                    <input type='hidden' name='saleItemID' value='$saleItemID'>
+                    <input type='hidden' name='saleItemCost' value='$price'>
+                    <button class= 'saleItem' type='submit'>$saleItemName <br> $displayPrice</button>
+                    
+                  </form>";
+        }
+        ?>
+    </span>
+
+    <span class='rightGUI'>
+        Test
+    </span>
+
+</div>
+
+
 </body>
 </html>
+
---
+++ process_buttonClick_saleItemAdd.php
@@ -0,0 +1,41 @@
+<?php
+/* Open Database Connection */
+include "database_connect.php";
+$connection = openConnection();
+
+session_start();
+$transactionID = $_SESSION['transactionID'];
+$companyName = $_SESSION['companyName'];
+
+$saleItemName = $_GET['saleItemName'];
+$cost = $_GET['saleItemCost'];
+
+$tableName = "transaction_".$transactionID."_".$companyName;
+
+$query = "SELECT Quantity FROM $tableName WHERE SaleItemName = '$saleItemName'";
+$result = mysqli_query($connection,$query);
+
+
+If (mysqli_num_rows($result) == 0){
+    $query = "INSERT INTO "."$tableName"."(SaleItemName,Cost,Quantity) VALUES('$saleItemName','$cost','1')";
+    mysqli_query($connection,$query);
+
+}
+else{
+    $row = mysqli_fetch_assoc($result);
+    $quantity = $row['Quantity'];
+
+    $newQuantity = $quantity + 1;
+    $query = "UPDATE $tableName 
+              SET Quantity = '$newQuantity' 
+              WHERE SaleItemName = '$saleItemName' ";
+    mysqli_query($connection,$query);
+
+}
+
+
+
+
+header('Location: pointOfSale.php');// redirect to home page
+exit;
+?>
