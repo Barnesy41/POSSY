1cf48c63f646b3d92e1efff9f934e3e595d03bdc
Barnesey41 <obarnes23@hotmail.co.uk>
2021-12-21 22:37:04 +0000

    Reverted to older version

--- pointOfSale.php
+++ pointOfSale.php
@@ -11,8 +11,6 @@
     <a href="MyMenus.php">MyMenus</a>
     <a href="stockManagement.php">Stock Management</a>
     <a class="current" href="pointOfSale.php">POS</a>
-    <a style="float: right;" href="Signup.php">Sign up</a>
-    <a style="float: right;" href="Login.php">Log-in</a>
 </div>
 
 <style>
