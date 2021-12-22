7c06c4f9373f7132c0fb5a38d71a3e38d239571a
Barnesey41 <obarnes23@hotmail.co.uk>
2021-12-22 12:11:28 +0000

    Implemented the daily total being calculated

--- dropdown.php
+++ dropdown.php
@@ -25,6 +25,35 @@
     <button type="submit" name="endOfDay">End Of Day</button>
 </form>
 <?php
+/* Open DB connection */
+include "database_connect.php";
+$connection = openConnection();
 
+/* Retrieve company name */
+$companyName = $_SESSION['companyName'];
 
-// echo "Daily Total: $total";
+/* Retrieve data from transaction history table where transaction is complete */
+$tableName = "transactionhistory_".$companyName; // Calculate table name
+$query = "SELECT * FROM $tableName WHERE Complete = 'yes'";
+$transactionhistoryResult = mysqli_query($connection,$query);
+
+/* Calculate number of returned rows */
+$numRows = 0;
+if($transactionhistoryResult == false){
+    $numRows = 0;
+}
+else{
+    $numRows = mysqli_num_rows($transactionhistoryResult);
+}
+
+/* Loop through each row of the results to the query, adding to the variable total the total cost of each
+ * transaction in the transaction history table that is complete
+ */
+$total = 0;
+for ($i=0; $i<$numRows; $i++){
+    $tableRow = mysqli_fetch_assoc($transactionhistoryResult);
+
+    $total = $total + $tableRow['Total'];
+}
+
+echo "Daily Total: ".$total; // Ouput the daily total
