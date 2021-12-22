690ff73421a434f3f6534e326219d9579b43d68e
Barnesey41 <obarnes23@hotmail.co.uk>
2021-12-22 11:54:36 +0000

    Included toolbar

--- dropdown.php
+++ dropdown.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="Styles.css" type="text/css"> <!-- link HomePage to style sheet -->
+</head>
+
+<html>
+<body>
+
+<div class="toolbar">
+    <a href="HomePage.php">Home</a>
+    <a href="MyMenus.php">MyMenus</a>
+    <a href="stockManagement.php">Stock Management</a>
+    <a href="pointOfSale.php">POS</a>
+    <?php session_start();
+    $username = $_SESSION['username']; // get username from login process
+    ?>
+    <a style="float: right;" class="current" href="dropdown.php"> <?php echo $username; ?></php></a>
+</div>
+
 <form action="refund.php">
     <button type="submit" name="refund">Refund</button>
 </form>
