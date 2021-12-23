412c00beffeae20d4e62fd7bd0c8023b077960e4
Barnesey41 <obarnes23@hotmail.co.uk>
2021-12-23 14:01:05 +0000

    Implemented Order Amount and Date Time input fields

--- lowStock.php
+++ lowStock.php
@@ -1,22 +1,33 @@
 <?php
-session_start();
+session_start(); // start the current session
 
 $row = $_SESSION['returnedRows'];
-$_SESSION['ProductID'] =  $row['ProductID'];
+$_SESSION['ProductID'] =  $row['ProductID']; // get the id of the current product
 
 echo "<head>
         <link rel='stylesheet' href='Styles.css' type='text/css'>
       </head>";
 
+/* Output data regarding product & supplier */
 echo "Product Name: ".$row['ProductName']."<br>Minimum Stock Value: ".$row['MinimumStockValue']."<br>".
      "Current Stock Value: ".$row['CurrentStockValue']."<br>Supplier Name: ".$row['SupplierName']."<br>".
      "Phone Number: ".$row['Phone'];
 
-echo " <form action='process_lowStock.php'>
-       <button type='submit'>Ordered</button>
-       </form> ";
+/* Output:
+ * Time & Date due input box
+ * Amount due input box
+ * Ordered form submit button
+*/
+echo " 
+    <form style='display: inline' action='process_lowStock.php'>
+    <br><br><label>Ordered Amount: <input type='number' name='orderAmount' step='1' value='0'></label><br>
+    <label>Due Date & Time: <input type='datetime-local' name='dateTime'></label><br>
+    <button type='submit'>Ordered</button> <!-- Output submit button -->
+    </form>
+    ";
 
-echo "<form class='lowStock' action='pointOfSale.php'>
+/* Output ignore submit button */
+echo "<form style='display: inline'  class='lowStock' action='pointOfSale.php'>
       <button type='submit' >Ignore</button>
       </form>";
 ?>
